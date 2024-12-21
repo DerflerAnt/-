11f751136438ee8a5b6f4c87efb54c2b2d2dcdf3
DerflerAnt <antonkovalenko095@gmail.com>
2024-12-21 21:20:08 +0200

    Add project files.

---
+++ Наслідування/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿// MainForm.Designer.cs
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+partial class MainForm
+{
+    private Button circleButton;
+    private Button squareButton;
+    private Button rhombButton;
+    private Button moveButton;
+    private Button stopButton;
+
+    private void InitializeComponent()
+    {
+        // Ініціалізація кнопок
+        this.circleButton = new Button();
+        this.squareButton = new Button();
+        this.rhombButton = new Button();
+        this.moveButton = new Button();
+        this.stopButton = new Button();
+
+        // circleButton
+        this.circleButton.Location = new Point(10, 10);
+        this.circleButton.Name = "circleButton";
+        this.circleButton.Size = new Size(75, 23);
+        this.circleButton.Text = "Circle";
+        this.circleButton.Click += new EventHandler(this.CircleButton_Click);
+        this.circleButton.BackColor = Color.White;
+
+        // squareButton
+        this.squareButton.Location = new Point(100, 10);
+        this.squareButton.Name = "squareButton";
+        this.squareButton.Size = new Size(75, 23);
+        this.squareButton.Text = "Square";
+        this.squareButton.Click += new EventHandler(this.SquareButton_Click);
+        this.squareButton.BackColor = Color.White;
+
+        // rhombButton
+        this.rhombButton.Location = new Point(200, 10);
+        this.rhombButton.Name = "rhombButton";
+        this.rhombButton.Size = new Size(75, 23);
+        this.rhombButton.Text = "Rhomb";
+        this.rhombButton.Click += new EventHandler(this.RhombButton_Click);
+        this.rhombButton.BackColor = Color.White;
+
+        // moveButton
+        this.moveButton.Location = new Point(380, 10);
+        this.moveButton.Name = "moveButton";
+        this.moveButton.Size = new Size(100, 23);
+        this.moveButton.Text = "Move Right";
+        this.moveButton.Click += new EventHandler(this.MoveButton_Click);
+        this.moveButton.BackColor = Color.Green;
+
+        // stopButton
+        this.stopButton.Location = new Point(500, 10);
+        this.stopButton.Name = "stopButton";
+        this.stopButton.Size = new Size(75, 23);
+        this.stopButton.Text = "Stop";
+        this.stopButton.Click += new EventHandler(this.StopButton_Click);
+        this.stopButton.BackColor = Color.Red;
+
+        // MainForm
+        this.ClientSize = new Size(800, 600);
+        this.Controls.Add(this.circleButton);
+        this.Controls.Add(this.squareButton);
+        this.Controls.Add(this.rhombButton);
+        this.Controls.Add(this.moveButton);
+        this.Controls.Add(this.stopButton);
+        this.Text = "Figures Example";
+        this.BackColor = Color.DarkGray;
+    }
+}
